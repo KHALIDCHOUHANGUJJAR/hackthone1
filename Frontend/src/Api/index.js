c8ae67602d683244fd5bf8d001265646ae3a2053
khalid chouhan <khalidchuhan7886@gmail.com>
2025-01-26 08:32:35 +0500

    main

--- Frontend/src/Api/index.js
+++ Frontend/src/Api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const ApiClient = axios.create({
-    baseURL: "http://localhost:3000/api",
-    timeout: 30000,
+    baseURL: "https://hackthone1-zn3v.vercel.app/api",
+    timeout: 10000,
     headers:{
         "Content-Type": "application/json"
     }
